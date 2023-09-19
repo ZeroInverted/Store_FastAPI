5e60b64f2415bdb4f9a37937ad96b111f59c09aa
Mahmoud Fathy <58611801+ZeroInverted@users.noreply.github.com>
2023-09-19 21:54:15 +0300

    Update db's path to be relative, db can be used as added test data

--- database.py
+++ database.py
@@ -2,16 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
 
 
-
-
-DATABASE_URL = "sqlite:///D:/API_Training/Store_FastAPI/store.db"
+DATABASE_URL = "sqlite:///store.db"
 
 engine = create_engine(DATABASE_URL)
 
-#Scoped sessions for thread-local sessions and easy session instantiation through db_session()
+# Scoped sessions for thread-local sessions and easy session instantiation through db_session()
 db_session = scoped_session(sessionmaker(bind=engine))
 
 Base = declarative_base()
+
 
 def get_db() -> db_session:
     db = db_session()
@@ -19,4 +18,3 @@ def get_db() -> db_session:
         yield db
     finally:
         db.close()
-
--- services/product_service.py
+++ services/product_service.py
@@ -3,24 +3,29 @@ from models.product_model import Product
 from schemas.product_schema import ProductInput
 from schemas.response_schema import Product_API_Response
 from sqlalchemy.orm import Session
-#GET
+# GET
 # skip and limit for pagination
-def get_all_products(db: Session, skip: int =0, limit: int =0, cat_id: int =0) -> Product_API_Response:
-    if cat_id ==0:
+
+
+def get_all_products(db: Session, skip: int = 0, limit: int = 0, cat_id: int = 0) -> Product_API_Response:
+    if cat_id == 0:
         products = db.query(Product).offset(skip).limit(limit).all()
-        if products is not None:    
+        if products is not None:
             return Product_API_Response(success=True, results=products)
         else:
-            error=["No products with such a category id."]
+            error = ["No products with such a category id."]
             return Product_API_Response(success=False, messages=error, status_code=404)
     else:
-        products =  db.query(Product).filter(Product.category_id==cat_id).offset(skip).limit(limit)
-        if products is not None:    
+        products = db.query(Product).filter(
+            Product.category_id == cat_id).offset(skip).limit(limit)
+        if products is not None:
             return Product_API_Response(success=True, results=products)
         else:
-            error=["No products found."]
+            error = ["No products found."]
             return Product_API_Response(success=False, messages=error, status_code=404)
-#POST
+# POST
+
+
 def create_product(db: Session, product: ProductInput) -> Product_API_Response:
     # serialize category input as dict, and then use ** to unpack dict to use as params.
     new_product = Product(**(product.model_dump()))
@@ -29,12 +34,13 @@ def create_product(db: Session, product: ProductInput) -> Product_API_Response:
         db.commit()
         db.refresh(new_product)
         return Product_API_Response(success=True, results=new_product)
-    except Exception as e:
-        print(e.format())
-        error=["Error creating product"]
+    except:
+        error = ["Error creating product"]
         return Product_API_Response(success=False, messages=error, status_code=409)
 
-#GET{id}
+# GET{id}
+
+
 def get_product_by_id(db: Session, id: int) -> Product_API_Response:
     product = db.query(Product).filter(Product.id == id).first()
     if product is None:
@@ -44,19 +50,19 @@ def get_product_by_id(db: Session, id: int) -> Product_API_Response:
         return Product_API_Response(success=True, results=product)
 
 
-#PUT{id}
-def put_product(db:Session, id: int, new_product: Product) -> Product_API_Response:
-    product = db.query(Product).filter(Product.id==id).first()
+# PUT{id}
+def put_product(db: Session, id: int, new_product: Product) -> Product_API_Response:
+    product = db.query(Product).filter(Product.id == id).first()
     if product is None:
         error = [f"Product with id: {id} is not found"]
-        return APIResponse(success=False, messages=error, status_code=404)
+        return Product_API_Response(success=False, messages=error, status_code=404)
     else:
         for key, value in new_product.dict().items():
             setattr(product, key, value)
     try:
         db.commit()
         db.refresh(product)
-        return APIResponse(success=True, results=product)
+        return Product_API_Response(success=True, results=product)
     except:
-        error=[f"Error updating resource of id: {id}"]
-        return APIResponse(success=False, messages=error, status_code=409)
+        error = [f"Error updating resource of id: {id}"]
+        return Product_API_Response(success=False, messages=error, status_code=409)
