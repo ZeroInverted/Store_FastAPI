5c15bcd91cb30cf3ca41153c3cd43da67d1514b4
Mahmoud Fathy <mahmoud.fathy@itworx.com>
2023-09-19 14:07:16 +0300

    Update schemas folder hierarchy

--- schemas/product_schema.py
+++ schemas/product_schema.py
@@ -1,17 +1,4 @@
 from pydantic import BaseModel
-
-class CategoryBase(BaseModel):
-    name: str
-
-class CategoryInput(CategoryBase):
-    pass
-
-class Category(CategoryBase):
-    id: int
-
-    class Config:
-        orm_mode = True
-
 class ProductBase(BaseModel):
     name: str
     price: float
