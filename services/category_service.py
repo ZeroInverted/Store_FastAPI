885d8bc1c137649d40faf527ee4adc18e0cf4af4
Mahmoud Fathy <mahmoud.fathy@itworx.com>
2023-09-19 14:24:37 +0300

    Add category services

---
+++ services/category_service.py
@@ -0,0 +1,14 @@
+from models.category_model import Category
+from schemas.category_schema import CategoryInput
+from sqlalchemy.orm import Session
+# skip and limit for pagination
+def get_all_categories(db: Session, skip: int =0, limit: int =0):
+    return db.query(Category).offset(skip).limit(limit).all()
+
+def create_Category(db: Session, category: CategoryInput):
+    # serialize category input as dict, and then use ** to unpack dict to use as params.
+    new_category = Category(**(category.model_dump()))
+    db.add(new_category)
+    db.commit()
+    db.refresh(new_category)
+    return new_category
